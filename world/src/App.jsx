debc7b0e1474e81eeef05269a4984f3cb62c4b43
AlainDLC <jd_alain@hotmail.com>
2024-08-21 16:20:28 +0200

    fake server and data

--- world/src/App.jsx
+++ world/src/App.jsx
@@ -1,5 +1,58 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import "./index.css";
+import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
 function App() {
-  return <div>WorldWide</div>;
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const BASE_URL = "http://localhost:8081";
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("Error component in progress");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CityList cities={cities} isLoiading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoiading={isLoading} />}
+          />
+          <Route path="countries" element={<p>countries</p>} />
+          <Route path="form" element={<p>form</p>} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ world/src/components/CityList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./CityList.module.css";
+import Spinner from "../components/Spinner";
+import CityItem from "./CityItem";
+import Message from "./Message";
+
+export default function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  if (!cities)
+    return <Message message="Just click and and city in our map!!" />;
+
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
+}
